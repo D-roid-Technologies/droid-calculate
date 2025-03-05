d4fe9b2f01b3da28324825fe89b0fe116140ff4b
developerekene <developerekene@gmail.com>
2025-03-05 11:39:59 +0000

    This is a new commit

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { StyleSheet } from "react-native";
+import React, { useEffect } from "react";
+import { StatusBar, StyleSheet, useColorScheme } from "react-native";
 import ScientificCalculator from "./src/app/screens/ScientificCalculator";
 import OhmsLawCalculator from "./src/app/screens/OhmsLawCalculator";
 import { NavigationContainer } from "@react-navigation/native";
@@ -7,6 +7,16 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import BMICalculator from "./src/app/screens/BMICalculator";
 
 export default function App() {
+  const theme = useColorScheme();
+
+  useEffect(() => {
+    StatusBar.setHidden(false); // Ensure the status bar is visible
+    if (theme === 'dark') {
+      StatusBar.setBarStyle('light-content'); // Light content (white text) for dark theme
+    } else {
+      StatusBar.setBarStyle('dark-content'); // Dark content (black text) for light theme
+    }
+  }, [theme]);
 
   const StackNavigator = createNativeStackNavigator()
   return (
@@ -24,54 +34,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-// Function to check if a button is an operator
-const isOperator = (value: string) => ["C", "DEL", "=", "+", "-", "*", "/", "sin", "cos", "tan", "log", "√", "x²"].includes(value);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#222",
-    paddingHorizontal: 10,
-  },
-  display: {
-    width: "100%",
-    backgroundColor: "#333",
-    padding: 20,
-    borderRadius: 10,
-    alignItems: "flex-end",
-    marginBottom: 20,
-  },
-  inputText: {
-    fontSize: 32,
-    color: "#fff",
-  },
-  resultText: {
-    fontSize: 24,
-    color: "#0f0",
-  },
-  buttonsContainer: {
-    width: "100%",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "center",
-  },
-  button: {
-    width: "22%",
-    height: 80,
-    margin: 5,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#444",
-    borderRadius: 10,
-  },
-  buttonText: {
-    fontSize: 28,
-    color: "#fff",
-  },
-  operatorButton: {
-    backgroundColor: "#2B3E98",
-  },
-});
